f87658ee68c0d2bca8f7c961132999f6ba763d2a
Aleksander Gorecki <aleksandergorecki80@gmail.com>
2018-04-12 07:29:27 +0100

    Init of git

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var 
+	a = prompt("Type base's width"),
+	h = prompt("Type height"),
+	triangleArea = a*h/2;
+	
+	console.log('Triangle field with base a:' + a + ' and height h:' + h + ' is equal to: ' + triangleArea);
